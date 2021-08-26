5e9fee31e2f378b5e787570bb52e93d6eb7594e3
Jabir K H <khjabir@gmail.com>
2021-08-26 16:49:20 +0530

    students screen added

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import { Button } from "antd";
+import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const Router = useRouter();
+
+  const goToStudentsScreen = () => {
+    Router.push("students");
+  };
+
   return (
     <>
       <h1>Hello World</h1>
-      <Button type="primary" onClick={() => alert("Hello World")}>
+      <Button type="primary" onClick={() => goToStudentsScreen()}>
         Click Me
       </Button>
     </>
